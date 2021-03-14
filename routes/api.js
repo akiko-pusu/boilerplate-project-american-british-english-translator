3c0b9cc08fb9509f51d884b7c04cf179268a25fe
Akiko Takano <akiko.pusu@gmail.com>
2021-03-14 13:09:26 +0900

    Apply first impl.

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,27 @@ const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
 
+  // Translatorを生成
   const translator = new Translator();
 
+  /*
+    // sample data
+    {
+        locale: "british-to-american",
+        text: "We had a party at my friend's condo."
+    }
+  */
   app.route('/api/translate')
     .post((req, res) => {
-      console.log('request');
+      let {
+        locale,
+        text
+      } = req.body;
+
+      const translated = translator.translate(text, locale);
+
       res.status(200)
-      .type('json')
-      .send('{ "translation": "Apple" }');
+        .type('json')
+        .send({ "translation": translated });
     });
 };
